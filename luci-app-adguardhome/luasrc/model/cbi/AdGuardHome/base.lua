76812a35406916b201ab00760b2eeb2f7234afe0
bcseputetto <bcseputetto@gmail.com>
2024-04-22 08:55:52 +0800

    Update base.lua

--- luci-app-adguardhome/luasrc/model/cbi/AdGuardHome/base.lua
+++ luci-app-adguardhome/luasrc/model/cbi/AdGuardHome/base.lua
@@ -56,7 +56,7 @@ o.template = "AdGuardHome/AdGuardHome_check"
 o.showfastconfig=(not fs.access(configpath))
 o.description = string.format(translate("Current core version:") .. "<strong><font id='updateversion' style=\'color:green\'>%s </font></strong>", e)
 ---- port warning not safe
-local port=luci.sys.exec("awk '/  port:/{printf($2);exit;}' "..configpath.." 2>nul")
+local port=luci.sys.exec("awk '/port:/ && ++count == 2 {sub(/[^0-9]+/, "", $2); print $2; exit}' "..configpath.." 2>nul")
 if (port=="") then port="?" end
 ---- Redirect
 o = s:option(ListValue, "redirect", port..translate("Redirect"), translate("AdGuardHome redirect mode"))
